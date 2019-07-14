b4661c60e009544f8151233cdfce1a14f624bf2a
Aleksander Piotrowski <apiotrowski312@gmail.com>
2019-07-14 09:49:15 +0200

    #1 add script for update

---
+++ auto-update-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+get_and_extract_hash () {
+  local hash
+
+  if [[ $# == 0 ]]; then
+    hash=$(md5sum /dev/stdin | cut -d ' ' -f 1)
+  else
+    hash=$(md5sum $1 | cut -d ' ' -f 1)
+  fi
+
+  echo "$hash"
+}
+
+(
+  # $1 - file to check
+  # $2 - link to download file
+
+  local_md5=$(get_and_extract_hash "$1")
+  curl_md5=$(curl -sL "$2" | get_and_extract_hash)
+
+  if [[ "$local_md5" != "$curl_md5" ]]; then
+    echo problem
+  else
+    echo "Newest version already installed"
+  fi
+)
